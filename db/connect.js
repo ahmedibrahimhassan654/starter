7c5f8bee63fde1369fb73bc142164be7bf1c4f81
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-12-02 22:55:34 +0200

    add mongo db to the project

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-}
+  });
 
-module.exports = connectDB
+  console.log(`MongoDB Connected`);
+};
+
+module.exports = connectDB;
